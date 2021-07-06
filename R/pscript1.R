2fc38fc82ab02107802ea868214c23117c71db9e
mheffe3 <mheffe3@uic.edu>
2021-07-06 12:53:59 -0500

    Updated the fibonacci and MatrixMultiplier functions to return rather than print the results and updated their documentation

--- R/pscript1.R
+++ R/pscript1.R
@@ -1,21 +1,23 @@
-fibonacci <- function(numTerms){
-  count = 0
+fibonacci <- function(n){
   a = 0
   b = 1
-  while(count < numTerms)
+  count = 0
+  sequence <- c()
+  while(count < n)
   {
-    print(a)
-    temp = a
-    a = b
-    b = temp+b
-    count = count+1
+    sequence <- append(sequence, a)
+    temp <- a
+    a <- b
+    b <- temp + b
+    count <- count + 1
   }
+  return(sequence)
 }
 fibonacci(8)
 
 
 MatrixMultiplier <- function(a,b){
   result <- a * b
-  print(result)
+  return(result)
 }
 MatrixMultiplier(M1 <- matrix(c(1,2,3,4), nrow = 2, byrow = TRUE),M2 <- matrix(c(1,2,3,4), nrow = 2, byrow = TRUE))
